1740e73f036d9a6c319aa5983472bca36c651e56
VasilijeBursac <vasilijebursac@gmail.com>
2020-11-03 03:55:28 +0100

    feat[g7]: Displaying basic info(object name) on map

--- HealthcareSystem/GraphicalEditor/Models/MapObjectRelated/MapObjectType.cs
+++ HealthcareSystem/GraphicalEditor/Models/MapObjectRelated/MapObjectType.cs
@@ -39,5 +39,55 @@ namespace GraphicalEditor.Models.MapObjectRelated
                     return Brushes.Yellow;
             }
         }
+
+        public string getObjectTypeFullName()
+        {
+            switch(TypeOfMapObject)
+            {
+                case MapObjectTypes.BUILDING:
+                    return "Zgrada";
+                case MapObjectTypes.EXAMINATION_ROOM:
+                    return "Soba za pregled";
+                case MapObjectTypes.OPERATION_ROOM:
+                    return "Operaciona sala";
+                case MapObjectTypes.WAITING_ROOM:
+                    return "Čekaonica";
+                case MapObjectTypes.PARKING:
+                    return "Parking";
+                case MapObjectTypes.RESTAURANT:
+                    return "Restoran";
+                case MapObjectTypes.HOSPITALIZATION_ROOM:
+                    return "Soba za oporavak";
+                case MapObjectTypes.WC:
+                    return "WC";
+                default:
+                    return "Objekat";
+            }
+        }
+
+        public string getObjectTypeNameAbbreviation()
+        {
+            switch (TypeOfMapObject)
+            {
+                case MapObjectTypes.BUILDING:
+                    return "Z";
+                case MapObjectTypes.EXAMINATION_ROOM:
+                    return "SP";
+                case MapObjectTypes.OPERATION_ROOM:
+                    return "OS";
+                case MapObjectTypes.WAITING_ROOM:
+                    return "Č";
+                case MapObjectTypes.PARKING:
+                    return "P";
+                case MapObjectTypes.RESTAURANT:
+                    return "R";
+                case MapObjectTypes.HOSPITALIZATION_ROOM:
+                    return "SO";
+                case MapObjectTypes.WC:
+                    return "WC";
+                default:
+                    return "O";
+            }
+        }
     }
 }

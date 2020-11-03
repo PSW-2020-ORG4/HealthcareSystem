3ca004cd2d1f8d661e680d51ce97b788c19434d2
Dragana Carapic <dragana.carapic98@uns.ac.rs>
2020-11-03 20:16:17 +0100

    feat[w1.1]: create frontend for add feedback

--- HealthcareSystem/PatientWebApp/Startup.cs
+++ HealthcareSystem/PatientWebApp/Startup.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Backend.Model;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -30,10 +32,13 @@ namespace PatientWebApp
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseStaticFiles();
 
             app.UseRouting();
 
@@ -43,6 +48,7 @@ namespace PatientWebApp
             {
                 endpoints.MapControllers();
             });
+
         }
     }
 }

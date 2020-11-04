02f08930c373265ea257d883e3080f5143f51e20
jelenabudisa <jelena.budisa@uns.ac.rs>
2020-11-04 21:04:14 +0100

    feat[w1.1]:adding feedback controller,adatpter and dto

---
+++ HealthcareSystem/PatientWebApp/Controllers/FeedbackController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Backend.Service.NotificationSurveyAndFeedback;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Model.Users;
+using PatientWebApp.Adapters;
+using PatientWebApp.DTOs;
+
+namespace PatientWebApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FeedbackController : ControllerBase
+    {
+        private readonly IFeedbackService _feedbackService;
+        public FeedbackController(IFeedbackService feedbackService)
+        {
+            _feedbackService = feedbackService;
+        }
+
+        [HttpPost("addFeedback")]
+        public void AddFeedback(FeedbackDTO feedbackDTO)
+        {
+            Feedback feedback = FeedbackAdapter.FeedbackDTOToFeedback(feedbackDTO);
+            _feedbackService.AddFeedback(feedback);
+        }
+    }
+}

7e0a733a226501e95a2a614d8b5af22dbdd536fd
jelena <jelenazeljko@live.com>
2020-11-04 23:27:10 +0100

    feat[w1.3]: create html file for unpublished feedback

---
+++ HealthcareSystem/PatientWebApp/wwwroot/js/unpublished_feedback.js
@@ -0,0 +1,21 @@
+﻿$(document).ready(function () {
+
+	let comment1 = $('<div class="border_comment">'
+		+ '<table class="table_comment">'
+		+ '<tr><th style="width:350px;">Patient:</th><td>Pera Peric</td></tr>'
+		+ '<tr><th>Comment:</th><td>Very good!</td></tr>'
+		+ '<tr><td style="width:250px;"><span></span></td><td style="width:250px;"><span></span></td>'
+		+ '<td><button class="btn yes"  onclick="approveComment(this.id)"><i class="fas fa-check"></i></button/></td>'
+		+ '</tr></table></div>');
+	$('div#div_comments').append(comment1);
+
+	let comment2 = $('<div class="border_comment">'
+		+ '<table class="table_comment">'
+		+ '<tr><th style="width:350px;">Patient:</th><td>Petra Peric</td></tr>'
+		+ '<tr><th>Comment:</th><td>Bad! :( </td></tr>'
+		+ '<tr><td style="width:250px;"><span></span></td><td style="width:250px;"><span></span></td>'
+		+ '<td><button class="btn yes"  onclick="approveComment(this.id)"><i class="fas fa-check"></i></button/></td>'
+		+ '</tr></table></div>');
+	$('div#div_comments').append(comment2);
+	
+});

304a37b04df761b70a034fe3ccce033c1f338900
jelena <jelenazeljko@live.com>
2020-11-06 14:15:49 +0100

    feat[w1.3]: modified page for unpublished feedbacks

--- HealthcareSystem/PatientWebApp/wwwroot/js/unpublished_feedback.js
+++ HealthcareSystem/PatientWebApp/wwwroot/js/unpublished_feedback.js
@@ -1,21 +1,69 @@
 ﻿$(document).ready(function () {
 
-	let comment1 = $('<div class="border_comment">'
-		+ '<table class="table_comment">'
-		+ '<tr><th style="width:350px;">Patient:</th><td>Pera Peric</td></tr>'
-		+ '<tr><th>Comment:</th><td>Very good!</td></tr>'
-		+ '<tr><td style="width:250px;"><span></span></td><td style="width:250px;"><span></span></td>'
-		+ '<td><button class="btn yes"  onclick="approveComment(this.id)"><i class="fas fa-check"></i></button/></td>'
-		+ '</tr></table></div>');
-	$('div#div_comments').append(comment1);
+	$.ajax({
+		url: "/api/feedback/unpublished-feedbacks",
+		type: "GET",
+		dataType: 'json',
+		processData: false,
+		contentType: false,
+		success: function (data) {
+			let length = data.length;
+			for (let i = 0; i < length; i++) {
+				addCommentTable(data[i]);
+			}
+		},
+		error: function () {
+			console.log('error getting comments');
+		}
+	});
 
-	let comment2 = $('<div class="border_comment">'
-		+ '<table class="table_comment">'
-		+ '<tr><th style="width:350px;">Patient:</th><td>Petra Peric</td></tr>'
-		+ '<tr><th>Comment:</th><td>Bad! :( </td></tr>'
-		+ '<tr><td style="width:250px;"><span></span></td><td style="width:250px;"><span></span></td>'
-		+ '<td><button class="btn yes"  onclick="approveComment(this.id)"><i class="fas fa-check"></i></button/></td>'
-		+ '</tr></table></div>');
-	$('div#div_comments').append(comment2);
-	
 });
+
+
+
+function addCommentTable(c) {
+	
+	let divElement = $('<div class= "border_comment"> <table class="table_comment"> '
+		+ ' <tr> <th style="width:350px;"><p style="margin-left:50px;">Patient:</p></th><td>' + c.commentatorName + ' ' + c.commentatorSurname + '</td></tr > '
+		+ ' <tr><th><p  style="margin-left:50px;">Comment:</p></th><td>' + c.comment + '</td></tr> '
+		+' </table ></div > ');
+
+	let trElement = $('<tr> <td style="width:250px;"><span></span></td> <td style = "width:250px;"><span></span></td> </tr>');
+
+	if (c.isAllowedToPublish) {
+		let tdElement = $('<td> <button style="color:green;" id="' + c.id + '" onclick="approveComment(this.id)"> Approve </button> </td>');
+		trElement.append(tdElement);
+	}
+	else {
+		let tdElement = $('<td style="color:red;"> Publishing disabled </td>');
+		trElement.append(tdElement);	
+	}
+	divElement.append(trElement);
+	$('div#div_comments').append(divElement);
+
+}
+
+
+
+function approveComment(feedbackId) {
+
+	alert(feedbackId);
+	console.log(feedbackId);
+
+	$.ajax({
+		type: "PUT",
+		url: "/api/feedback/",
+		data: feedbackId,
+		success: function () {
+			alert("Success! Approved comment with id " + feedbackId);
+			setTimeout(function () {
+				location.reload();
+			}, 500);
+		},
+		error: function () {
+			alert("error approving comment");
+		}
+	});
+
+}
+

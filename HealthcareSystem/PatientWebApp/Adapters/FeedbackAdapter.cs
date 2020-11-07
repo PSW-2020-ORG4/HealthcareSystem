39ba5b55a3d18e2795050c60889a77003090fdff
jelena <jelenazeljko@live.com>
2020-11-07 18:35:28 +0100

    refactor[w1.4]: modified feedback class

--- HealthcareSystem/PatientWebApp/Adapters/FeedbackAdapter.cs
+++ HealthcareSystem/PatientWebApp/Adapters/FeedbackAdapter.cs
@@ -2,6 +2,7 @@
 using PatientWebApp.DTOs;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -26,9 +27,19 @@ namespace PatientWebApp.Adapters
             FeedbackDTO dto = new FeedbackDTO();
             dto.Comment = feedback.Comment;
             dto.Id = feedback.Id;
+            dto.SendingDate = feedback.SendingDate.ToString("dd/MM/yyyy");
+
             dto.CommentatorJmbg = feedback.CommentatorJmbg;
-            dto.CommentatorName = feedback.Commentator.Name;
-            dto.CommentatorSurname = feedback.Commentator.Surname;
+            if (dto.CommentatorJmbg == null)
+            {
+                dto.CommentatorName = "";
+                dto.CommentatorSurname = "";
+            }
+            else
+            {
+                dto.CommentatorName = feedback.Commentator.Name;
+                dto.CommentatorSurname = feedback.Commentator.Surname;
+            }
             dto.IsAllowedToPublish = feedback.IsAllowedToPublish;
 
             return dto;

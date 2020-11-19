6e5380a32e302fc23243502bbf5f6049812b9c7c
Sladjana Savkovic <sladjana.savkovic97@uns.ac.rs>
2020-11-19 19:04:25 +0100

    gui[w2.1]:create html, css and js page for patient registration

---
+++ HealthcareSystem/PatientWebApp/wwwroot/js/patient_registration.js
@@ -0,0 +1,33 @@
+﻿$(document).ready(function () {
+
+	$('#image_row').empty();
+
+	//choose profile image
+	var chosen_image = [];
+	if (window.File && window.FileList && window.FileReader) {
+
+		$("#add_image").on("change", function (e) {
+
+			var files = e.target.files,
+				filesLength = files.length;
+
+			for (var i = 0; i < filesLength; i++) {
+				var f = files[i];
+				chosen_image.push(f);
+				var fileReader = new FileReader();
+				fileReader.onload = (function (e) {
+					var column = $("<div  class=\"col-md-3\"><span class=\"pip\">" +
+						"<img class=\"imageThumb\" style='width:100%; height:90%; margin-top:10px;' src=\"" + e.target.result + "\" title=\"" + f.name + "\"/>"
+						+ "</span></div>");
+					$('#image_row').append(column);
+
+
+				});
+				fileReader.readAsDataURL(f);
+			}
+		});
+	} else {
+		alert("Your browser doesn't support to File API")
+	}
+
+});

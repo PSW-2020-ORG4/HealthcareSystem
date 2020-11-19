3fcfaed05025ea769576caf5b84004a0042ee0e0
Sladjana Savkovic <sladjana.savkovic97@uns.ac.rs>
2020-11-19 21:49:07 +0100

    gui[w2.1]:changing variable names in js file

--- HealthcareSystem/PatientWebApp/wwwroot/js/patient_registration.js
+++ HealthcareSystem/PatientWebApp/wwwroot/js/patient_registration.js
@@ -2,32 +2,26 @@
 
 	$('#image_row').empty();
 
-	//choose profile image
+	/* Display the image on the html page */
 	var chosen_image = [];
 	if (window.File && window.FileList && window.FileReader) {
 
 		$("#add_image").on("change", function (e) {
 
-			var files = e.target.files,
-				filesLength = files.length;
-
-			for (var i = 0; i < filesLength; i++) {
-				var f = files[i];
-				chosen_image.push(f);
-				var fileReader = new FileReader();
-				fileReader.onload = (function (e) {
-					var column = $("<div  class=\"col-md-3\"><span class=\"pip\">" +
-						"<img class=\"imageThumb\" style='width:100%; height:90%; margin-top:10px;' src=\"" + e.target.result + "\" title=\"" + f.name + "\"/>"
-						+ "</span></div>");
-					$('#image_row').append(column);
-
-
-				});
-				fileReader.readAsDataURL(f);
-			}
+			var images = e.target.files, filesLength = images.length;
+			var image = images[0];
+			chosen_image.push(image);
+			var fileReader = new FileReader();
+			fileReader.onload = (function (e) {
+				var image_div = $("<div class=\"col-md-3\"><span class=\"pip\">" +
+					"<img class=\"imageThumb\" style='width:100%; height:90%; margin-top:10px;' src=\"" + e.target.result + "\" title=\"" + image.name + "\"/>"
+					+ "</span></div>");
+				$('#image_row').append(image_div);
+			});
+			fileReader.readAsDataURL(image);
 		});
 	} else {
-		alert("Your browser doesn't support to File API")
+		alert("Your browser doesn't support chosen file")
 	}
 
 });

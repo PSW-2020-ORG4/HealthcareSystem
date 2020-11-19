e77d8ae53ef26f0f0ffb88b5b1b30d5517d65d48
Sladjana Savkovic <sladjana.savkovic97@uns.ac.rs>
2020-11-19 16:37:25 +0100

    refactor[w2.1]:adding country class and refactor relationships

--- HealthcareSystem/Backend/Model/Users/City.cs
+++ HealthcareSystem/Backend/Model/Users/City.cs
@@ -4,9 +4,11 @@
  * Purpose: Definition of the Class Korisnik.City
  ***********************************************************************/
 
+using Backend;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Model.Users
 {
@@ -15,18 +17,22 @@ namespace Model.Users
         [Key]
         public int ZipCode { get; set; }
         public string Name { get; set; }
+
+        [ForeignKey("Country")]
+        public int CountryId { get; set; }
+        public virtual Country Country { get; set; }
         public virtual ICollection<User> Users { get; set; }
 
         public City() { }
         public City(string name,int zipCode)
         {
-            this.Name = name;
-            this.ZipCode = zipCode;
+            Name = name;
+            ZipCode = zipCode;
         }
         public City(City city)
         {
-            this.Name = city.Name;
-            this.ZipCode = city.ZipCode;
+            Name = city.Name;
+            ZipCode = city.ZipCode;
         }
     }
 }

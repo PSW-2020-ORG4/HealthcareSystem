037bb6ba089d764b52db00b658ded2e2f53ac707
Sladjana Savkovic <sladjana.savkovic97@uns.ac.rs>
2020-11-21 10:53:45 +0100

    feat[w2.3]:adding test for patient get method

--- HealthcareSystem/PatientWebAppUnitTests/PatientControllerTests.cs
+++ HealthcareSystem/PatientWebAppUnitTests/PatientControllerTests.cs
@@ -8,6 +8,7 @@ using Xunit;
 using Model.Enums;
 using Microsoft.AspNetCore.Mvc;
 using Backend;
+using System.Collections.Generic;
 
 namespace PatientWebAppUnitTests
 {
@@ -16,26 +17,32 @@ namespace PatientWebAppUnitTests
         private IActivePatientRepository CreatePatientStubRepository()
         {
             var patientStubRepository = new Mock<IActivePatientRepository>();
-            var patient = new Patient(jmbg: "1234567891234", name: "Pera", surname: "Peric", dateOfBirth: DateTime.Now, gender: GenderType.M,
+            var patients = new List<Patient>();
+
+            patients.Add(new Patient(jmbg: "1234567891234", name: "Pera", surname: "Peric", dateOfBirth: DateTime.Now, gender: GenderType.M,
                             city: new City(zipCode: 21000, name: "Novi Sad", country: new Country(id: 1, name: "Serbia")),
                             homeAddress: "Zmaj Jovina 10", phone: "065452102", email: "pera@gmail.com", username: "pera",
-                            password: "pera", dateOfRegistration: DateTime.Now, isGuest: false);
-            patientStubRepository.Setup(m => m.GetPatientByJmbg("1234567891234")).Returns(patient);
+                            password: "pera", dateOfRegistration: DateTime.Now, isGuest: false));
+
+            patientStubRepository.Setup(m => m.GetPatientByJmbg("1234567891234")).Returns(patients[0]);
             return patientStubRepository.Object;
         }
 
         private IActivePatientCardRepository CreatePatientCardStubRepository()
         {
             var patientCardStubRepository = new Mock<IActivePatientCardRepository>();
-            var patientCard = new PatientCard(id: 1, bloodType: BloodType.A, rhFactor: RhFactorType.Negativna, alergies: "",
-                                   medicalHistory: "", hasInsurance: false, lbo: null, patientJmbg: "1234567891234");
-            patientCardStubRepository.Setup(m => m.GetPatientCardByJmbg("1234567891234")).Returns(patientCard);
+            var patientCards = new List<PatientCard>();
+
+            patientCards.Add(new PatientCard(id: 1, bloodType: BloodType.A, rhFactor: RhFactorType.Negativna, alergies: "",
+                                   medicalHistory: "", hasInsurance: false, lbo: null, patientJmbg: "1234567891234"));
+
+            patientCardStubRepository.Setup(m => m.GetPatientCardByJmbg("1234567891234")).Returns(patientCards[0]);
             return patientCardStubRepository.Object;
 
         }
 
         [Fact]
-        public void Get_patient_by_jmbg()
+        public void Get_existent_patient_by_jmbg()
         {
             PatientService patientService = new PatientService(CreatePatientStubRepository());
             PatientCardService patientCardService = new PatientCardService(CreatePatientCardStubRepository());
@@ -44,6 +51,18 @@ namespace PatientWebAppUnitTests
             var result = patientController.GetPatientByJmbg("1234567891234") as ObjectResult;
 
             Assert.True(result is OkObjectResult);
+        }
+
+        [Fact]
+        public void Get_non_existent_patient_by_jmbg()
+        {
+            PatientService patientService = new PatientService(CreatePatientStubRepository());
+            PatientCardService patientCardService = new PatientCardService(CreatePatientCardStubRepository());
+            PatientController patientController = new PatientController(patientService, patientCardService);
+
+            var result = patientController.GetPatientByJmbg("1054789652001") as ObjectResult;
+
+            Assert.True(result is NotFoundObjectResult);
         }
     }
 }

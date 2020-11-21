c05a1e46de4a9e9711da1e63f15802df327151a7
Sladjana Savkovic <sladjana.savkovic97@uns.ac.rs>
2020-11-21 11:52:16 +0100

    feat[w2.1]:creating tests for add patient method

--- HealthcareSystem/PatientWebAppUnitTests/PatientControllerTests.cs
+++ HealthcareSystem/PatientWebAppUnitTests/PatientControllerTests.cs
@@ -9,6 +9,7 @@ using Model.Enums;
 using Microsoft.AspNetCore.Mvc;
 using Backend;
 using System.Collections.Generic;
+using PatientWebApp.DTOs;
 
 namespace PatientWebAppUnitTests
 {
@@ -25,6 +26,8 @@ namespace PatientWebAppUnitTests
                             password: "pera", dateOfRegistration: DateTime.Now, isGuest: false));
 
             patientStubRepository.Setup(m => m.GetPatientByJmbg("1234567891234")).Returns(patients[0]);
+            patientStubRepository.Setup(m => m.AddPatient(new Patient()));
+
             return patientStubRepository.Object;
         }
 
@@ -37,8 +40,30 @@ namespace PatientWebAppUnitTests
                                    medicalHistory: "", hasInsurance: false, lbo: null, patientJmbg: "1234567891234"));
 
             patientCardStubRepository.Setup(m => m.GetPatientCardByJmbg("1234567891234")).Returns(patientCards[0]);
+            patientCardStubRepository.Setup(m => m.AddPatientCard(new PatientCard()));
+
             return patientCardStubRepository.Object;
 
+        }
+
+        private PatientDTO CreateValidPatientDTOForTestingAddMethod()
+        {
+            PatientDTO patientDTO = new PatientDTO(name:"Ana",surname:"Anic",jmbg:"1206988452102", gender:GenderType.Z,
+                                                    dateOfBirth:DateTime.Now,phone:"065432485",countryId:1,countryName:"Srbija",
+                                                    cityZipCode:21000,cityName:"Novi Sad",homeAddress:"Bulevar Oslobodjenja 10", 
+                                                    bloodType:BloodType.A,rhFactor:RhFactorType.Negativna,hasInsurance:false,lbo:"",
+                                                    alergies:"",medicalHistory:"",email:"ana@gmail.com",password:"ana");
+            return patientDTO;
+        }
+
+        private PatientDTO CreateInvalidPatientDTOForTestingAddMethod()
+        {
+            PatientDTO patientDTO = new PatientDTO(name: "Ana", surname: "Anic", jmbg: null, gender: GenderType.Z,
+                                                    dateOfBirth: DateTime.Now, phone: "", countryId: 1, countryName: "Srbija",
+                                                    cityZipCode: 21000, cityName: "Novi Sad", homeAddress: null,
+                                                    bloodType: BloodType.A, rhFactor: RhFactorType.Negativna, hasInsurance: false, lbo: "",
+                                                    alergies: null, medicalHistory: null, email: null, password: "");
+            return patientDTO;
         }
 
         [Fact]
@@ -63,6 +88,30 @@ namespace PatientWebAppUnitTests
             var result = patientController.GetPatientByJmbg("1054789652001") as ObjectResult;
 
             Assert.True(result is NotFoundObjectResult);
+        }
+
+        [Fact]
+        public void Add_valid_patient()
+        {
+            PatientService patientService = new PatientService(CreatePatientStubRepository());
+            PatientCardService patientCardService = new PatientCardService(CreatePatientCardStubRepository());
+            PatientController patientController = new PatientController(patientService, patientCardService);
+            
+            var result = patientController.AddPatient(CreateValidPatientDTOForTestingAddMethod());
+
+            Assert.True(result is OkResult);
+        }
+
+        [Fact]
+        public void Add_invalid_patient()
+        {
+            PatientService patientService = new PatientService(CreatePatientStubRepository());
+            PatientCardService patientCardService = new PatientCardService(CreatePatientCardStubRepository());
+            PatientController patientController = new PatientController(patientService, patientCardService);
+
+            var result = patientController.AddPatient(CreateInvalidPatientDTOForTestingAddMethod());
+
+            Assert.True(result is BadRequestObjectResult);
         }
     }
 }

ba19e00e8e1fc56824a0e731412f2cafdc3c5403
draganacarapic <dragana.carapic98@uns.ac.rs>
2020-11-23 14:21:42 +0100

    gui[w2.2]: edit html and js files

--- HealthcareSystem/PatientWebApp/wwwroot/js/activate_account.js
+++ HealthcareSystem/PatientWebApp/wwwroot/js/activate_account.js
@@ -1,6 +1,4 @@
 ﻿$(document).ready(function () {
-    $("#activate").click(function (e) {
-        e.preventDefault();
 
         let url_split = window.location.href.split("?")[1];
 
@@ -22,5 +20,30 @@
                 alert(jqXHR.responseText);
             }
         });
+
+    $("#activate").click(function (e) {
+     e.preventDefault();
+
+    let url_split = window.location.href.split("?")[1];
+
+    var jmbg = url_split.split("=")[1];
+
+    $.ajax({
+        type: "PUT",
+        url: "/api/patient/activate/" + jmbg,
+        success: function () {
+
+            setTimeout(function () {
+                window.location.href = 'patients_home_page.html';
+            }, 500);
+
+
+        },
+        error: function (jqXHR) {
+
+            alert(jqXHR.responseText);
+        }
     });
+    });
+    
 });

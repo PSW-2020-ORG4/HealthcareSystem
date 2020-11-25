15784d7066d04567fabb77fb4816fc7851c420e0
borislav-puzic <borislavpuzic@gmail.com>
2020-11-25 21:02:42 +0100

    feat[ia3.1]: added background service for receiving rabbitmq action and benefit messages

---
+++ HealthcareSystem/Backend/Service/ActionBenefitService/RabbitMqActionBenefitService/RabbitMqActionBenefitService.cs
@@ -0,0 +1,138 @@
+﻿using Backend.Model.Pharmacies;
+using Backend.Service.Pharmacies;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Backend.Service
+{
+    public class RabbitMqActionBenefitMessageingService : BackgroundService
+    {
+        private IModel _channel;
+        private IConnection _connection;
+        private readonly IServiceProvider _service;
+        private readonly String _hostname;
+        private readonly String _username;
+        private readonly String _password;
+        private readonly String _queueName;
+
+        public RabbitMqActionBenefitMessageingService(IServiceProvider service, IOptions<RabbitMqConfiguration> rabbitMqOPtions)
+        {
+            _service = service;
+            _hostname = rabbitMqOPtions.Value.Hostname;
+            _username = rabbitMqOPtions.Value.Username;
+            _password = rabbitMqOPtions.Value.Password;
+            _queueName = "bolnica-1";
+            InitializeRabbitMqListener();
+        }
+
+        private void InitializeRabbitMqListener()
+        {
+            var factory = new ConnectionFactory() { HostName = _hostname, UserName = _username, Password = _password };
+            _connection = factory.CreateConnection();
+            _connection.ConnectionShutdown += RabbitMQ_ConnectionShutdown;
+            _channel = _connection.CreateModel();
+            _channel.QueueDeclare(queue: _queueName,
+                                  durable: true,
+                                  exclusive: false,
+                                  autoDelete: false,
+                                  arguments: null);
+
+            using (var scope = _service.CreateScope())
+            {
+                IPharmacyService pharmacyService = scope.ServiceProvider.GetRequiredService<IPharmacyService>();
+
+                foreach (Pharmacy p in pharmacyService.GetPharmaciesBySubscribed(true))
+                {
+                    _channel.ExchangeDeclare(exchange: p.ActionsBenefitsExchangeName, type: ExchangeType.Fanout);
+                    _channel.QueueBind(queue: _queueName, exchange: p.ActionsBenefitsExchangeName, routingKey: "");
+                }
+            }
+        }
+
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            stoppingToken.ThrowIfCancellationRequested();
+
+            var consumer = new EventingBasicConsumer(_channel);
+            consumer.Received += OnConsumerReceived;
+            consumer.Shutdown += OnConsumerShutdown;
+            consumer.Registered += OnConsumerRegistered;
+            consumer.Unregistered += OnConsumerUnregistered;
+            consumer.ConsumerCancelled += OnConsumerCancelled;
+
+            _channel.BasicConsume(queue: _queueName, autoAck: false, consumer: consumer);
+
+            return Task.CompletedTask;
+        }
+
+        public void Unsubscribe(String exchangeName)
+        {
+            _channel.QueueUnbind(queue: _queueName, exchange: exchangeName, "", null);
+        }
+
+        private void HandleMessage(string message, string exchangeName)
+        {
+            using (var scope = _service.CreateScope())
+            {
+                IActionBenefitService actionBenefitService = scope.ServiceProvider.GetRequiredService<IActionBenefitService>();
+
+                actionBenefitService.CreateActionBenefit(exchangeName, message);
+            }
+        }
+
+        private void RabbitMQ_ConnectionShutdown(object sender, ShutdownEventArgs e)
+        {
+
+        }
+
+        private void OnConsumerReceived(object sender, BasicDeliverEventArgs e)
+        {
+            var body = e.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            var exchangeName = e.Exchange;
+
+            HandleMessage(message, exchangeName);
+            _channel.BasicAck(e.DeliveryTag, false);
+        }
+
+        private void OnConsumerShutdown(object sender, ShutdownEventArgs e)
+        {
+            //throw new NotImplementedException();
+        }
+
+        private void OnConsumerRegistered(object sender, ConsumerEventArgs e)
+        {
+            //throw new NotImplementedException();
+        }
+
+        private void OnConsumerUnregistered(object sender, ConsumerEventArgs e)
+        {
+            //throw new NotImplementedException();
+        }
+
+        private void OnConsumerCancelled(object sender, ConsumerEventArgs e)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public override void Dispose()
+        {
+            if (_channel != null)
+                _channel.Close();
+
+            if (_connection != null)
+                _connection.Close();
+
+            base.Dispose();
+        }
+    }
+}

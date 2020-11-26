dd21f43b166073602beea5287372214c6a72db90
Stefan <stefankitanovic@hotmail.com>
2020-11-26 13:54:09 +0100

    test[ia5.2]: added tests for api registration in the controller

---
+++ HealthcareSystem/IntegrationAdaptersTests/UnitTests/ApiRegisterTests.cs
@@ -0,0 +1,55 @@
+﻿using Backend.Repository;
+using IntegrationAdapters.Controllers;
+using IntegrationAdaptersTests.DataFactory;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Xunit;
+using Moq;
+
+namespace IntegrationAdaptersTests.UnitTests
+{
+    public class ApiRegisterTests
+    {
+        [Fact]
+        public void Successfully_registers_api()
+        {
+            var pharmacy = CreatePharmacy.CreateValidTestObject();
+            var stubRepository = new Mock<IPharmacyRepo>();
+            stubRepository.Setup(m => m.CreatePharmacy(pharmacy));
+            var tempData = new TempDataDictionary(new DefaultHttpContext(), Mock.Of<ITempDataProvider>())
+            {
+                ["Success"] = "Registration successful!"
+            };
+            var controller = new HomeController(stubRepository.Object)
+            {
+                TempData = tempData
+            };
+
+            var result = controller.ApiRegister(pharmacy);
+
+            Assert.IsType<RedirectToActionResult>(result);
+        }
+
+        [Fact]
+        public void Does_not_successfully_register_api()
+        {
+            var pharmacy = CreatePharmacy.CreateInvalidTestObject();
+            var stubRepository = new Mock<IPharmacyRepo>();
+            stubRepository.Setup(m => m.CreatePharmacy(pharmacy));
+            var tempData = new TempDataDictionary(new DefaultHttpContext(), Mock.Of<ITempDataProvider>())
+            {
+                ["Success"] = "Registration successful!"
+            };
+            var controller = new HomeController(stubRepository.Object)
+            {
+                TempData = tempData
+            };
+            controller.ModelState.AddModelError("Url", "Required");
+
+            var result = controller.ApiRegister(pharmacy);
+
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
+    }
+}

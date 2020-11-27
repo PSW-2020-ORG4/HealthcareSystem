ba52b090ee872f69fbb6cd0e17cd8ce02350ab1b
jelena <jelenazeljko@live.com>
2020-11-27 05:14:59 +0100

    refactor[w2.7]: modified methods in survey repository

--- HealthcareSystem/Backend/Repository/SurveyRepository/MySqlSurveyRepository/MySqlSurveyRepository.cs
+++ HealthcareSystem/Backend/Repository/SurveyRepository/MySqlSurveyRepository/MySqlSurveyRepository.cs
@@ -27,7 +27,53 @@ namespace Backend.Repository
         public List<SurveyResult> GetSurveyResultsAboutDoctor(string jmbg)
         {
             List<SurveyResult> surveyResults = new List<SurveyResult>();
+            SurveyResult behaviorOfDoctor = getSurveyResultAboutBehaviorOfDoctor(jmbg);          
+            SurveyResult doctorProfessionalism = getSurveyResultAboutDoctorProfessionalism(jmbg);
+            SurveyResult gettingAdviceByDoctor = getSurveyResultAboutGettingAdviceByDoctor(jmbg);
+            SurveyResult availabilityOfDoctor = getSurveyResultAboutAvailabilityOfDoctor(jmbg);
+            surveyResults.Add(behaviorOfDoctor);
+            surveyResults.Add(doctorProfessionalism);
+            surveyResults.Add(gettingAdviceByDoctor);
+            surveyResults.Add(availabilityOfDoctor);
+            return surveyResults;
+        }      
 
+        public List<SurveyResult> GetSurveyResultsAboutHospital()
+        {
+            List<SurveyResult> surveyResults = new List<SurveyResult>();
+            SurveyResult nursing = getSurveyResultAboutNursing();
+            SurveyResult cleanliness = getSurveyResultAboutCleanliness();
+            SurveyResult overallRating = getSurveyResultAboutOverallRating();
+            SurveyResult satisfactionWithDrugAndInstrument = getSurveyResultAboutDrugAndInstrument();
+            surveyResults.Add(nursing);
+            surveyResults.Add(cleanliness);
+            surveyResults.Add(overallRating);
+            surveyResults.Add(satisfactionWithDrugAndInstrument);
+            return surveyResults;
+        }   
+
+        public List<SurveyResult> GetSurveyResultsAboutMedicalStaff()
+        {
+            List<SurveyResult> surveyResults = new List<SurveyResult>();
+            SurveyResult behaviorOfMedicalStaff = getSurveyResultAboutBehaviorOfMedicalStaff();
+            SurveyResult medicalStaffProfessionalism = getSurveyResultAboutMedicalStaffProfessionalism();
+            SurveyResult gettingAdviceByMedicalStaff = getSurveyResultAboutGettingAdviceByMedicalStaff();
+            SurveyResult easeInObtainingFollowupInformation = getSurveyResultAboutEaseInObtainingFollowUpInformation();
+            surveyResults.Add(behaviorOfMedicalStaff);
+            surveyResults.Add(medicalStaffProfessionalism);
+            surveyResults.Add(gettingAdviceByMedicalStaff);
+            surveyResults.Add(easeInObtainingFollowupInformation);
+           
+            return surveyResults;
+        }
+
+        public List<SurveyAboutDoctor> GetSurveysByDoctor(string jmbg)
+        {
+            return _context.SurveysAboutDoctor.Where(x => x.DoctorJmbg.Equals(jmbg)).ToList();
+        }
+
+        private SurveyResult getSurveyResultAboutBehaviorOfDoctor(string jmbg)
+        {
             SurveyResult behaviorOfDoctor = new SurveyResult();
             behaviorOfDoctor.RatedItem = "Behavior of doctor";
             behaviorOfDoctor.AverageRating = GetSurveysByDoctor(jmbg).Average(x => x.BehaviorOfDoctor);
@@ -36,7 +82,11 @@ namespace Backend.Repository
             behaviorOfDoctor.NumberOfGradesThree = GetSurveysByDoctor(jmbg).Count(x => x.BehaviorOfDoctor == 3);
             behaviorOfDoctor.NumberOfGradesFour = GetSurveysByDoctor(jmbg).Count(x => x.BehaviorOfDoctor == 4);
             behaviorOfDoctor.NumberOfGradesFive = GetSurveysByDoctor(jmbg).Count(x => x.BehaviorOfDoctor == 5);
-            surveyResults.Add(behaviorOfDoctor);
+            return behaviorOfDoctor;
+        }
+
+        private SurveyResult getSurveyResultAboutDoctorProfessionalism(string jmbg)
+        {
             SurveyResult doctorProfessionalism = new SurveyResult();
             doctorProfessionalism.RatedItem = "Professionalism";
             doctorProfessionalism.AverageRating = GetSurveysByDoctor(jmbg).Average(x => x.DoctorProfessionalism);
@@ -45,7 +95,11 @@ namespace Backend.Repository
             doctorProfessionalism.NumberOfGradesThree = GetSurveysByDoctor(jmbg).Count(x => x.DoctorProfessionalism == 3);
             doctorProfessionalism.NumberOfGradesFour = GetSurveysByDoctor(jmbg).Count(x => x.DoctorProfessionalism == 4);
             doctorProfessionalism.NumberOfGradesFive = GetSurveysByDoctor(jmbg).Count(x => x.DoctorProfessionalism == 5);
-            surveyResults.Add(doctorProfessionalism);
+            return doctorProfessionalism;
+        }
+
+        private SurveyResult getSurveyResultAboutGettingAdviceByDoctor(string jmbg)
+        {
             SurveyResult gettingAdviceByDoctor = new SurveyResult();
             gettingAdviceByDoctor.RatedItem = "Getting advice by doctor";
             gettingAdviceByDoctor.AverageRating = GetSurveysByDoctor(jmbg).Average(x => x.GettingAdviceByDoctor);
@@ -54,7 +108,11 @@ namespace Backend.Repository
             gettingAdviceByDoctor.NumberOfGradesThree = GetSurveysByDoctor(jmbg).Count(x => x.GettingAdviceByDoctor == 3);
             gettingAdviceByDoctor.NumberOfGradesFour = GetSurveysByDoctor(jmbg).Count(x => x.GettingAdviceByDoctor == 4);
             gettingAdviceByDoctor.NumberOfGradesFive = GetSurveysByDoctor(jmbg).Count(x => x.GettingAdviceByDoctor == 5);
-            surveyResults.Add(gettingAdviceByDoctor);
+            return gettingAdviceByDoctor;
+        }
+
+        private SurveyResult getSurveyResultAboutAvailabilityOfDoctor(string jmbg)
+        {
             SurveyResult availabilityOfDoctor = new SurveyResult();
             availabilityOfDoctor.RatedItem = "Availability of doctor";
             availabilityOfDoctor.AverageRating = GetSurveysByDoctor(jmbg).Average(x => x.AvailabilityOfDoctor);
@@ -63,15 +121,11 @@ namespace Backend.Repository
             availabilityOfDoctor.NumberOfGradesThree = GetSurveysByDoctor(jmbg).Count(x => x.AvailabilityOfDoctor == 3);
             availabilityOfDoctor.NumberOfGradesFour = GetSurveysByDoctor(jmbg).Count(x => x.AvailabilityOfDoctor == 4);
             availabilityOfDoctor.NumberOfGradesFive = GetSurveysByDoctor(jmbg).Count(x => x.AvailabilityOfDoctor == 5);
-            surveyResults.Add(availabilityOfDoctor);
-
-            return surveyResults;
+            return availabilityOfDoctor;
         }
 
-        public List<SurveyResult> GetSurveyResultsAboutHospital()
+        private SurveyResult getSurveyResultAboutNursing()
         {
-            List<SurveyResult> surveyResults = new List<SurveyResult>();
-
             SurveyResult nursing = new SurveyResult();
             nursing.RatedItem = "Nursing";
             nursing.AverageRating = _context.SurveysAboutHospital.Average(x => x.Nursing);
@@ -80,7 +134,11 @@ namespace Backend.Repository
             nursing.NumberOfGradesThree = _context.SurveysAboutHospital.Count(x => x.Nursing == 3);
             nursing.NumberOfGradesFour = _context.SurveysAboutHospital.Count(x => x.Nursing == 4);
             nursing.NumberOfGradesFive = _context.SurveysAboutHospital.Count(x => x.Nursing == 5);
-            surveyResults.Add(nursing);
+            return nursing;
+        }
+
+        private SurveyResult getSurveyResultAboutCleanliness()
+        {
             SurveyResult cleanliness = new SurveyResult();
             cleanliness.RatedItem = "Cleanliness";
             cleanliness.AverageRating = _context.SurveysAboutHospital.Average(x => x.Cleanliness);
@@ -89,7 +147,11 @@ namespace Backend.Repository
             cleanliness.NumberOfGradesThree = _context.SurveysAboutHospital.Count(x => x.Cleanliness == 3);
             cleanliness.NumberOfGradesFour = _context.SurveysAboutHospital.Count(x => x.Cleanliness == 4);
             cleanliness.NumberOfGradesFive = _context.SurveysAboutHospital.Count(x => x.Cleanliness == 5);
-            surveyResults.Add(cleanliness);
+            return cleanliness;
+        }
+
+        private SurveyResult getSurveyResultAboutOverallRating()
+        {
             SurveyResult overallRating = new SurveyResult();
             overallRating.RatedItem = "Overall rating";
             overallRating.AverageRating = _context.SurveysAboutHospital.Average(x => x.OverallRating);
@@ -98,24 +160,24 @@ namespace Backend.Repository
             overallRating.NumberOfGradesThree = _context.SurveysAboutHospital.Count(x => x.OverallRating == 3);
             overallRating.NumberOfGradesFour = _context.SurveysAboutHospital.Count(x => x.OverallRating == 4);
             overallRating.NumberOfGradesFive = _context.SurveysAboutHospital.Count(x => x.OverallRating == 5);
-            surveyResults.Add(overallRating);
-            SurveyResult satisfiedWithDrugAndInstrument = new SurveyResult();
-            satisfiedWithDrugAndInstrument.RatedItem = "Satisfied with drug and instrument";
-            satisfiedWithDrugAndInstrument.AverageRating = _context.SurveysAboutHospital.Average(x => x.SatisfiedWithDrugAndInstrument);
-            satisfiedWithDrugAndInstrument.NumberOfGradesOne = _context.SurveysAboutHospital.Count(x => x.SatisfiedWithDrugAndInstrument == 1);
-            satisfiedWithDrugAndInstrument.NumberOfGradesTwo = _context.SurveysAboutHospital.Count(x => x.SatisfiedWithDrugAndInstrument == 2);
-            satisfiedWithDrugAndInstrument.NumberOfGradesThree = _context.SurveysAboutHospital.Count(x => x.SatisfiedWithDrugAndInstrument == 3);
-            satisfiedWithDrugAndInstrument.NumberOfGradesFour = _context.SurveysAboutHospital.Count(x => x.SatisfiedWithDrugAndInstrument == 4);
-            satisfiedWithDrugAndInstrument.NumberOfGradesFive = _context.SurveysAboutHospital.Count(x => x.SatisfiedWithDrugAndInstrument == 5);
-            surveyResults.Add(satisfiedWithDrugAndInstrument);
-
-            return surveyResults;
+            return overallRating;
         }
 
-        public List<SurveyResult> GetSurveyResultsAboutMedicalStaff()
+        private SurveyResult getSurveyResultAboutDrugAndInstrument()
         {
-            List<SurveyResult> surveyResults = new List<SurveyResult>();
+            SurveyResult satisfactionWithDrugAndInstrument = new SurveyResult();
+            satisfactionWithDrugAndInstrument.RatedItem = "Satisfaction with drug and instrument";
+            satisfactionWithDrugAndInstrument.AverageRating = _context.SurveysAboutHospital.Average(x => x.SatisfiedWithDrugAndInstrument);
+            satisfactionWithDrugAndInstrument.NumberOfGradesOne = _context.SurveysAboutHospital.Count(x => x.SatisfiedWithDrugAndInstrument == 1);
+            satisfactionWithDrugAndInstrument.NumberOfGradesTwo = _context.SurveysAboutHospital.Count(x => x.SatisfiedWithDrugAndInstrument == 2);
+            satisfactionWithDrugAndInstrument.NumberOfGradesThree = _context.SurveysAboutHospital.Count(x => x.SatisfiedWithDrugAndInstrument == 3);
+            satisfactionWithDrugAndInstrument.NumberOfGradesFour = _context.SurveysAboutHospital.Count(x => x.SatisfiedWithDrugAndInstrument == 4);
+            satisfactionWithDrugAndInstrument.NumberOfGradesFive = _context.SurveysAboutHospital.Count(x => x.SatisfiedWithDrugAndInstrument == 5);
+            return satisfactionWithDrugAndInstrument;
+        }
 
+        private SurveyResult getSurveyResultAboutBehaviorOfMedicalStaff()
+        {
             SurveyResult behaviorOfMedicalStaff = new SurveyResult();
             behaviorOfMedicalStaff.RatedItem = "Behavior of medical staff";
             behaviorOfMedicalStaff.AverageRating = _context.SurveysAboutMedicalStaff.Average(x => x.BehaviorOfMedicalStaff);
@@ -124,7 +186,11 @@ namespace Backend.Repository
             behaviorOfMedicalStaff.NumberOfGradesThree = _context.SurveysAboutMedicalStaff.Count(x => x.BehaviorOfMedicalStaff == 3);
             behaviorOfMedicalStaff.NumberOfGradesFour = _context.SurveysAboutMedicalStaff.Count(x => x.BehaviorOfMedicalStaff == 4);
             behaviorOfMedicalStaff.NumberOfGradesFive = _context.SurveysAboutMedicalStaff.Count(x => x.BehaviorOfMedicalStaff == 5);
-            surveyResults.Add(behaviorOfMedicalStaff);
+            return behaviorOfMedicalStaff;
+        }
+
+        private SurveyResult getSurveyResultAboutMedicalStaffProfessionalism()
+        {
             SurveyResult medicalStaffProfessionalism = new SurveyResult();
             medicalStaffProfessionalism.RatedItem = "Professionalism of medical staff";
             medicalStaffProfessionalism.AverageRating = _context.SurveysAboutMedicalStaff.Average(x => x.MedicalStaffProfessionalism);
@@ -133,7 +199,11 @@ namespace Backend.Repository
             medicalStaffProfessionalism.NumberOfGradesThree = _context.SurveysAboutMedicalStaff.Count(x => x.MedicalStaffProfessionalism == 3);
             medicalStaffProfessionalism.NumberOfGradesFour = _context.SurveysAboutMedicalStaff.Count(x => x.MedicalStaffProfessionalism == 4);
             medicalStaffProfessionalism.NumberOfGradesFive = _context.SurveysAboutMedicalStaff.Count(x => x.MedicalStaffProfessionalism == 5);
-            surveyResults.Add(medicalStaffProfessionalism);
+            return medicalStaffProfessionalism;
+        }
+
+        private SurveyResult getSurveyResultAboutGettingAdviceByMedicalStaff()
+        {
             SurveyResult gettingAdviceByMedicalStaff = new SurveyResult();
             gettingAdviceByMedicalStaff.RatedItem = "Getting advice or help when needed from medical staff";
             gettingAdviceByMedicalStaff.AverageRating = _context.SurveysAboutMedicalStaff.Average(x => x.GettingAdviceByMedicalStaff);
@@ -142,7 +212,11 @@ namespace Backend.Repository
             gettingAdviceByMedicalStaff.NumberOfGradesThree = _context.SurveysAboutMedicalStaff.Count(x => x.GettingAdviceByMedicalStaff == 3);
             gettingAdviceByMedicalStaff.NumberOfGradesFour = _context.SurveysAboutMedicalStaff.Count(x => x.GettingAdviceByMedicalStaff == 4);
             gettingAdviceByMedicalStaff.NumberOfGradesFive = _context.SurveysAboutMedicalStaff.Count(x => x.GettingAdviceByMedicalStaff == 5);
-            surveyResults.Add(gettingAdviceByMedicalStaff);
+            return gettingAdviceByMedicalStaff;
+        }
+
+        private SurveyResult getSurveyResultAboutEaseInObtainingFollowUpInformation()
+        {
             SurveyResult easeInObtainingFollowupInformation = new SurveyResult();
             easeInObtainingFollowupInformation.RatedItem = "Ease in obtaining follow up information and care";
             easeInObtainingFollowupInformation.AverageRating = _context.SurveysAboutMedicalStaff.Average(x => x.EaseInObtainingFollowUpInformation);
@@ -151,14 +225,8 @@ namespace Backend.Repository
             easeInObtainingFollowupInformation.NumberOfGradesThree = _context.SurveysAboutMedicalStaff.Count(x => x.EaseInObtainingFollowUpInformation == 3);
             easeInObtainingFollowupInformation.NumberOfGradesFour = _context.SurveysAboutMedicalStaff.Count(x => x.EaseInObtainingFollowUpInformation == 4);
             easeInObtainingFollowupInformation.NumberOfGradesFive = _context.SurveysAboutMedicalStaff.Count(x => x.EaseInObtainingFollowUpInformation == 5);
-            surveyResults.Add(easeInObtainingFollowupInformation);
-
-            return surveyResults;
+            return easeInObtainingFollowupInformation;
         }
 
-        public List<SurveyAboutDoctor> GetSurveysByDoctor(string jmbg)
-        {
-            return _context.SurveysAboutDoctor.Where(x => x.DoctorJmbg.Equals(jmbg)).ToList();
-        }
     }
 }

d92766979dda68d31f46d770a328546af6627eea
borislav-puzic <borislavpuzic@gmail.com>
2020-11-27 16:20:36 +0100

    test[ia3.3]: added test

--- HealthcareSystem/IntegrationAdaptersTests/UnitTests/ActionBenefitControllerTest.cs
+++ HealthcareSystem/IntegrationAdaptersTests/UnitTests/ActionBenefitControllerTest.cs
@@ -30,5 +30,26 @@ namespace IntegrationAdaptersTests.UnitTests
             Assert.True(((List<ActionBenefit>)result.Model).Count == 1);
         }
 
+        [Fact]
+        public void MakePublic_CallActionBenefitServiceMakePublic()
+        {
+            var mockaActionBenefitService = new Mock<IActionBenefitService>();
+            mockaActionBenefitService.Setup(ab => ab.MakePublic(It.IsAny<int>(), It.Is<bool>(isPublic => isPublic == true))).Verifiable();
+            var controller = new ActionBenefitController(mockaActionBenefitService.Object);
+
+            controller.MakePublic(5, true);
+
+            mockaActionBenefitService.Verify();
+        }
+
+        [Fact]
+        public void MakePublic_CallActionBenefitServiceMakePublic_ThrowsArgumentException()
+        {
+            var mockaActionBenefitService = new Mock<IActionBenefitService>();
+            mockaActionBenefitService.Setup(ab => ab.MakePublic(It.IsAny<int>(), It.IsAny<bool>())).Throws(new ArgumentException());
+            var controller = new ActionBenefitController(mockaActionBenefitService.Object);
+
+            Assert.Throws<ArgumentException>(() => controller.MakePublic(5, true));
+        }
     }
 }

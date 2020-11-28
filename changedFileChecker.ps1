b8308d15d85dabe4f9340e9c4375ba71c0f9abae
milana-todorovic <milanatodorovic@uns.ac.rs>
2020-11-28 13:45:19 +0100

    moved powershell script to file

---
+++ changedFileChecker.ps1
@@ -0,0 +1,32 @@
+$reason = "$(Build.Reason)"
+if($reason -eq "PullRequest")
+{
+    $files=$(git diff HEAD HEAD~ --name-only)
+    $paths = $env:PATHS -split ' '
+            
+    $shouldBuild = $false
+    foreach ($path in $paths) 
+    { 
+        if( $files -like $path) 
+        {
+            $shouldBuild = $true
+            break
+        } 
+    }
+
+    if ($shouldBuild)
+    {
+        echo "Build will execute"
+        Write-Host "##vso[task.setvariable variable=shouldBuild;isOutput=true]Yes"
+    }
+    else
+    {
+        echo "Build will not execute"
+        Write-Host "##vso[task.setvariable variable=shouldBuild;isOutput=true]No"
+    }
+}
+else
+{
+    echo "Build will execute"
+    Write-Host "##vso[task.setvariable variable=shouldBuild;isOutput=true]Yes"
+}

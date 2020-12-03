e0f85e2e7681521eb770969e1fca60b2af8cfc0d
Sladjana Savkovic <sladjana.savkovic97@uns.ac.rs>
2020-12-03 14:33:39 +0100

    fix[w3.4]:resolve build errors

--- HealthcareSystem/PatientWebAppTests/CreateObjectsForTests/CreateExamination.cs
+++ HealthcareSystem/PatientWebAppTests/CreateObjectsForTests/CreateExamination.cs
@@ -34,7 +34,7 @@ namespace PatientWebAppTests.CreateObjectsForTests
             List<Examination> examinations = new List<Examination>();
             examinations.Add(new Examination(id: 1, typeOfExamination: TypeOfExamination.GENERAL, dateAndTime: new DateTime(2020, 11, 11), anamnesis: "Bol u grlu", doctor: new Doctor(jmbg: "0909965768767", name: "Ana", surname: "Markovic", dateOfBirth: DateTime.Now, gender: GenderType.F, city: new City(zipCode: 21000, name: "Novi Sad", country: new Country(id: 1, name: "Serbia")),
                             homeAddress: "Zmaj Jovina 10", phone: "065452102", email: "pera@gmail.com", username: "pera",
-                            password: "12345678", numberOfLicence: "", typeOfDoctor: TypeOfDoctor.drOpstePrakse, doctorsOffice: new Room(number: 1, typeOfUsage: TypeOfUsage.CONSULTING_ROOM,
+                            password: "12345678", numberOfLicence: "", doctorsOffice: new Room(number: 1, typeOfUsage: TypeOfUsage.CONSULTING_ROOM,
                 capacity: 1, occupation: 1, renovation: false), dateOfEmployment: DateTime.Now), room: new Room(number: 1, typeOfUsage: TypeOfUsage.CONSULTING_ROOM,
                 capacity: 1, occupation: 1, renovation: false),
                 patientCard: new PatientCard(id: 1, bloodType: BloodType.A, rhFactor: RhFactorType.NEGATIVE, alergies: "", medicalHistory: "",
@@ -42,7 +42,7 @@ namespace PatientWebAppTests.CreateObjectsForTests
 
             examinations.Add(new Examination(id: 2, typeOfExamination: TypeOfExamination.GENERAL, dateAndTime: new DateTime(2020, 12, 01), anamnesis: "COVID-19", doctor: new Doctor(jmbg: "0909965768767", name: "Ana", surname: "Markovic", dateOfBirth: DateTime.Now, gender: GenderType.F, city: new City(zipCode: 21000, name: "Novi Sad", country: new Country(id: 1, name: "Serbia")),
                             homeAddress: "Zmaj Jovina 10", phone: "065452102", email: "pera@gmail.com", username: "pera",
-                            password: "12345678", numberOfLicence: "", typeOfDoctor: TypeOfDoctor.drOpstePrakse, doctorsOffice: new Room(number: 1, typeOfUsage: TypeOfUsage.CONSULTING_ROOM,
+                            password: "12345678", numberOfLicence: "", doctorsOffice: new Room(number: 1, typeOfUsage: TypeOfUsage.CONSULTING_ROOM,
                 capacity: 1, occupation: 1, renovation: false), dateOfEmployment: DateTime.Now), room: new Room(number: 1, typeOfUsage: TypeOfUsage.CONSULTING_ROOM,
                 capacity: 1, occupation: 1, renovation: false),
                 patientCard: new PatientCard(id: 1, bloodType: BloodType.A, rhFactor: RhFactorType.NEGATIVE, alergies: "", medicalHistory: "",
@@ -50,7 +50,7 @@ namespace PatientWebAppTests.CreateObjectsForTests
 
             examinations.Add(new Examination(id: 3, typeOfExamination: TypeOfExamination.GENERAL, dateAndTime: new DateTime(2020, 12, 01), anamnesis: "Glavobolja", doctor: new Doctor(jmbg: "1254789652123", name: "Dara", surname: "Darić", dateOfBirth: DateTime.Now, gender: GenderType.F, city: new City(zipCode: 21000, name: "Novi Sad", country: new Country(id: 1, name: "Serbia")),
                             homeAddress: "Zmaj Jovina 10", phone: "065452102", email: "dara@gmail.com", username: "dara",
-                            password: "12345678", numberOfLicence: "", typeOfDoctor: TypeOfDoctor.drOpstePrakse, doctorsOffice: new Room(number: 2, typeOfUsage: TypeOfUsage.CONSULTING_ROOM,
+                            password: "12345678", numberOfLicence: "", doctorsOffice: new Room(number: 2, typeOfUsage: TypeOfUsage.CONSULTING_ROOM,
                 capacity: 1, occupation: 1, renovation: false), dateOfEmployment: DateTime.Now), room: new Room(number: 1, typeOfUsage: TypeOfUsage.CONSULTING_ROOM,
                 capacity: 1, occupation: 1, renovation: false),
                 patientCard: new PatientCard(id: 1, bloodType: BloodType.A, rhFactor: RhFactorType.NEGATIVE, alergies: "", medicalHistory: "",

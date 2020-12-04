01e25cb6b4c1d7be102e29fde99c6edd12c97d40
Pufke <pualic.nemanja@gmail.com>
2020-12-05 00:45:06 +0100

    chore: rabbitMq added in docker-compose

--- HealthcareSystem/IntegrationAdapters/Program.cs
+++ HealthcareSystem/IntegrationAdapters/Program.cs
@@ -1,5 +1,9 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
 
 namespace IntegrationAdapters
 {
@@ -8,6 +12,29 @@ namespace IntegrationAdapters
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "hello",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                string message = "Hello World!";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "hello",
+                                     basicProperties: null,
+                                     body: body);
+                Console.WriteLine(" [x] Sent {0}", message);
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>

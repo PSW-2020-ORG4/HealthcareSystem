3ab6131615f0c622be8be607941490fa0b9266ae
Sladjana Savkovic <sladjana.savkovic97@uns.ac.rs>
2020-12-13 23:09:46 +0100

    gui[]:edit style

--- HealthcareSystem/Backend/Service/ExaminationAndPatientCard/FreeAppointmentSearchService.cs
+++ HealthcareSystem/Backend/Service/ExaminationAndPatientCard/FreeAppointmentSearchService.cs
@@ -85,8 +85,8 @@ namespace Backend.Service.ExaminationAndPatientCard
 
         private ICollection<Examination> RelaxDates(AppointmentSearchWithPrioritiesDTO parameters)
         {
-            DateTime earliestDateTime = parameters.InitialParameters.EarliestDateTime.AddDays(-7);
-            DateTime latestDateTime = parameters.InitialParameters.LatestDateTime.AddDays(7);
+            DateTime earliestDateTime = parameters.InitialParameters.EarliestDateTime.AddDays(-5);
+            DateTime latestDateTime = parameters.InitialParameters.LatestDateTime.AddDays(5);
             parameters.InitialParameters.EarliestDateTime = earliestDateTime;
             parameters.InitialParameters.LatestDateTime = latestDateTime;
 

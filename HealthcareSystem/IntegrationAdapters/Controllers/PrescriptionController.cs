47fc796cee0c1b86923085642090b3aaeb79cafb
borislav-puzic <borislavpuzic@gmail.com>
2020-12-14 14:00:11 +0100

    feat[ia2.3]: empty view and controller for presctiption

---
+++ HealthcareSystem/IntegrationAdapters/Controllers/PrescriptionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IntegrationAdapters.Controllers
+{
+    public class PrescriptionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

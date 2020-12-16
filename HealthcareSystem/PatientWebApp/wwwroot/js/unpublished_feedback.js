1f9bfcc2be47b93f347da82d5dcc19a74eb53fa4
milana-todorovic <milanatodorovic@uns.ac.rs>
2020-12-16 11:44:18 +0100

    gui: improved user interaction when publishing feedback

--- HealthcareSystem/PatientWebApp/wwwroot/js/unpublished_feedback.js
+++ HealthcareSystem/PatientWebApp/wwwroot/js/unpublished_feedback.js
@@ -19,7 +19,8 @@
 			}
 		},
 		error: function () {
-			console.log('error getting comments');
+			let alert = $('<div class="alert alert-danger m-4" role="alert">Error fetching data.</div >')
+			$('div#view_feedbacks').prepend(alert);
 		}
 	});
 
@@ -32,7 +33,7 @@ function addCommentTable(feedback) {
 	}
 
 	if (feedback.isAllowedToPublish) {
-		let new_feedback = $('<div class="row"><div class="col p-4"><div class="card"><div class="card-header bg-info text-white">'
+		let new_feedback = $('<div class="row" id="r' + feedback.id + '"><div class="col p-4"><div class="card"><div class="card-header bg-info text-white">'
 			+ feedback.sendingDate
 			+ '</div>'
 			+ '<div class="card-body bg-light"><blockquote class="blockquote mb-0"><p>'
@@ -65,15 +66,15 @@ function approveComment(feedbackId) {
 		type: "PUT",
 		url: "/api/feedback/" + feedbackId,
 		success: function () {
-
-			console.log('You have successfully approved a feedback!');
-			setTimeout(function () {
-				location.reload();
-			}, 500);
+			let alert = $('<div class="alert alert-success alert-dismissible fade show m-4" role="alert">Feedback was successfully published.'
+				+ '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>' + '</div >')
+			$('div#view_feedbacks').prepend(alert);
+			$('#r' + feedbackId).remove();
 		},
 		error: function (jqXHR) {
-
-			alert(jqXHR.responseText);
+			let alert = $('<div class="alert alert-danger alert-dismissible fade show m-4" role="alert">Publishing was not successful.'
+				+ '<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>' + '</div >')
+            $('div#view_feedbacks').prepend(alert);
 		}
 	});
 }

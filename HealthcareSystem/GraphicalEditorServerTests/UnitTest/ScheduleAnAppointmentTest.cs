d35c4cd7f31d32f102066ddb3da04bc270e4e6f0
VasilijeBursac <vasilijebursac@gmail.com>
2020-12-20 23:31:41 +0100

    feat[g3.1-g3.6]: Tests for Appointment Searches and Appointment Scheduling

---
+++ HealthcareSystem/GraphicalEditorServerTests/UnitTest/ScheduleAnAppointmentTest.cs
@@ -0,0 +1,35 @@
+﻿using Backend.Service.ExaminationAndPatientCard;
+using GraphicalEditorServerTests.DataFactory;
+using Model.PerformingExamination;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+namespace GraphicalEditorServerTests.UnitTest
+{
+   public class ScheduleAnAppointmentTest
+    {
+        private readonly StubRepository _stubRepository;
+        private readonly CreateExamination _createExamination;
+
+        public ScheduleAnAppointmentTest()
+        {
+            _stubRepository = new StubRepository();
+            _createExamination = new CreateExamination();
+
+        }
+
+        public ScheduleAppointmentService SetupScheduleAppointmentService()
+        {
+          return new ScheduleAppointmentService(_stubRepository.CreateExaminationStubRepository());
+        }
+
+        [Fact]
+        public void Schedule_an_appointment()
+        {
+            ScheduleAppointmentService scheduleAppointmentService = SetupScheduleAppointmentService();
+            scheduleAppointmentService.ScheduleAnAppointmentByDoctor(_createExamination.CreateValidTestObjectForMakingAnAppointemnt());
+            Assert.True(scheduleAppointmentService.GetExaminationById(2).Id == 2);
+        }
+    }
+}

c94e44721a402e8fbf94b0a21a6c459eb6263340
jelena <jelenazeljko@live.com>
2020-12-25 23:20:38 +0100

    feat[w4.1]: redirecting to the home page

--- HealthcareSystem/PatientWebApp/wwwroot/js/login.js
+++ HealthcareSystem/PatientWebApp/wwwroot/js/login.js
@@ -26,7 +26,17 @@
 				data: JSON.stringify(userCredentialsDTO),
 				success: function (tokenUser) {
 					localStorage.setItem('token', tokenUser);
-					window.location.href = "patients_home_page.html";
+
+					const jwt = localStorage.getItem('token');
+					var decoded = parseJwt(jwt);
+					console.log(decoded);
+					
+					if (decoded.role == "Patient") {
+						window.location.href = "patients_home_page.html";
+					}
+					if (decoded.role == "Admin") {
+						window.location.href = "admins_home_page.html";
+					}
 				},
 				error: function (jqXHR) {
 					let alert = $('<div class="alert alert-danger alert-dismissible fade show m-1" role="alert"> Incorrect username and password'
@@ -37,7 +47,15 @@
 			});
 		}
 	});
-
 });
 
 
+function parseJwt(token) {
+	var base64Url = token.split('.')[1];
+	var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+	var jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
+		return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+	}).join(''));
+
+	return JSON.parse(jsonPayload);
+};

cef0a8ee470f177ba868801a408eb33d72dcd47e
draganacarapic <dragana.carapic98@uns.ac.rs>
2020-12-26 15:07:32 +0100

    gui[w4.2]: edit name of html page

--- HealthcareSystem/PatientWebApp/wwwroot/js/navbars/admin.js
+++ HealthcareSystem/PatientWebApp/wwwroot/js/navbars/admin.js
@@ -1,7 +1,7 @@
 ﻿$(document).ready(function () {
     $('body').prepend($(
         '<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">'
-        + '<a class="navbar-brand" href="/html/unpublished_feedback.html">HOSPITAL</a>'
+        + '<a class="navbar-brand" href="/html/admins_home_page.html">HOSPITAL</a>'
         + ' <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">'
         + '<span class="navbar-toggler-icon"></span>'
         + ' </button>'

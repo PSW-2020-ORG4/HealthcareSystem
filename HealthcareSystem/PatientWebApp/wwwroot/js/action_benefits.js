0481548a9fde9f590f810858727e528cc1b1fb89
draganacarapic <dragana.carapic98@uns.ac.rs>
2020-12-27 13:05:41 +0100

    gui[w4.1]: edit frontend

--- HealthcareSystem/PatientWebApp/wwwroot/js/action_benefits.js
+++ HealthcareSystem/PatientWebApp/wwwroot/js/action_benefits.js
@@ -31,21 +31,21 @@ function addAction(action, i) {
     if (i == 0) {
         text_image = $('<div class="carousel-item" style="background-image: url(/images/4.jpg);">'
             + '< img class= "d-block" src = "/images/4.jpg" alt = "First slide" >'
-            + '<div class="carousel-caption trickcenter"><h1 style="margin-left:140px;"><b>APOTEKA ' + action.pharmacyName + '</b></h1><h1 style="margin-left:160px;"><b>' + action.subject + '</b></h1><h3 style="margin-left:290px;"><b>' + action.message + '</b><h3></div></div>');
+            + '<div class="carousel-caption trickcenter"><h1 style="margin-left:140px;"><b>APOTEKA ' + action.pharmacyName + '</b></h1><h1 style="margin-left:160px;"><b>' + action.subject + '</b></h1><h3 style="margin-left:250px;"><b>' + action.message + '</b><h3></div></div>');
 
         $('div#action_benefit_div').append(text_image);
     }
     if (i == 1) {
         text_image = $('<div class="carousel-item" style="background-image: url(/images/5.jpg);">'
             + '< img class= "d-block" src = "/images/5.jpg" alt = "First slide" >'
-            + '<div class="carousel-caption trickcenter"><h1 style="margin-left:140px;"><b>APOTEKA ' + action.pharmacyName + '</b></h1><h1 style="margin-left:160px;"><b>' + action.subject + '</b></h1><h3 style="margin-left:290px;"><b>' + action.message + '</b><h3></div></div>');
+            + '<div class="carousel-caption trickcenter"><h1 style="margin-left:140px;"><b>APOTEKA ' + action.pharmacyName + '</b></h1><h1 style="margin-left:160px;"><b>' + action.subject + '</b></h1><h3 style="margin-left:250px;"><b>' + action.message + '</b><h3></div></div>');
 
         $('div#action_benefit_div').append(text_image);
     }
     if (i == 2) {
         text_image = $('<div class="carousel-item" style="background-image: url(/images/6.jpg);">'
             + '< img class= "d-block" src = "/images/6.jpg" alt = "First slide" >'
-            + '<div class="carousel-caption trickcenter"><h1 style="margin-left:140px;"><b>APOTEKA ' + action.pharmacyName + '</b></h1><h1 style="margin-left:160px;"><b>' + action.subject + '</b></h1><h3 style="margin-left:290px;"><b>' + action.message + '</b><h3></div></div>');
+            + '<div class="carousel-caption trickcenter"><h1 style="margin-left:140px;"><b>APOTEKA ' + action.pharmacyName + '</b></h1><h1 style="margin-left:160px;"><b>' + action.subject + '</b></h1><h3 style="margin-left:250px;"><b>' + action.message + '</b><h3></div></div>');
 
         $('div#action_benefit_div').append(text_image);
     }

a9a94b8bffa43eca4f6373c2d756f21b43fcced5
milana-todorovic <milanatodorovic@uns.ac.rs>
2021-01-04 18:48:26 +0100

    test[d68, w4.3]: specification tests moved to PatientServiceTests

---
+++ HealthcareSystem/PatientServiceTests/UnitTests/CompositeSpecificationTests.cs
@@ -0,0 +1,35 @@
+﻿using Moq;
+using PatientService.Model.Specification;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace PatientServiceTests.UnitTests
+{
+    public class CompositeSpecificationTests
+    {
+        [Theory]
+        [InlineData(true, true, true, LogicalOperator.And)]
+        [InlineData(true, false, false, LogicalOperator.And)]
+        [InlineData(false, true, false, LogicalOperator.And)]
+        [InlineData(false, false, false, LogicalOperator.And)]
+        [InlineData(true, true, true, LogicalOperator.Or)]
+        [InlineData(true, false, true, LogicalOperator.Or)]
+        [InlineData(false, true, true, LogicalOperator.Or)]
+        [InlineData(false, false, false, LogicalOperator.Or)]
+        public void LogicalOperationTests(bool left, bool right, bool expected, LogicalOperator logOp)
+        {
+            var leftMock = new Mock<CompositeSpecification<bool>>();
+            leftMock.Setup(m => m.IsSatisfiedBy(true)).Returns(left);
+
+            var rightMock = new Mock<CompositeSpecification<bool>>();
+            rightMock.Setup(m => m.IsSatisfiedBy(true)).Returns(right);
+
+            ISpecification<bool> uut = leftMock.Object.BinaryOperation(logOp, rightMock.Object);
+            bool result = uut.IsSatisfiedBy(true);
+
+            Assert.Equal(result, expected);
+        }
+    }
+}

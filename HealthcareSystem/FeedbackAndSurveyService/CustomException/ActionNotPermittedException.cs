4cd6c03b752861771601bdaf13ac7c5380fbfab7
milana-todorovic <milanatodorovic@uns.ac.rs>
2021-01-05 16:43:15 +0100

    refactor[d69, w4.3]: exceptions added

---
+++ HealthcareSystem/FeedbackAndSurveyService/CustomException/ActionNotPermittedException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FeedbackAndSurveyService.CustomException
+{
+    public class ActionNotPermittedException : FeedbackAndSurveyServiceException
+    {
+        public ActionNotPermittedException() : base() { }
+
+        public ActionNotPermittedException(string message) : base(message) { }
+    }
+}

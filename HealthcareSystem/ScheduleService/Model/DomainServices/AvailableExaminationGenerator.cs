1d4d93ce8271e8cafd1d42306d5c913e4cf7d74a
Sladjana Savkovic <sladjana.savkovic97@uns.ac.rs>
2021-01-08 18:20:49 +0100

    refactor[w4.3]:domain services added

--- HealthcareSystem/ScheduleService/Model/DomainServices/AvailableExaminationGenerator.cs
+++ HealthcareSystem/ScheduleService/Model/DomainServices/AvailableExaminationGenerator.cs
@@ -1,15 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ScheduleService.Model.DomainServices
 {
     public class AvailableExaminationGenerator : IAvailableExaminationGenerator
     {
-        public IEnumerable<Examination> Generate(ExaminationGeneratorDTO examinationDTO)
+        public ICollection<Examination> Generate(ExaminationGeneratorDTO examinationDTO)
         {
-            throw new NotImplementedException();
+            ICollection<Examination> examinations = new List<Examination>();
+            ICollection<Appointment> appointments = GenerateAppointments(examinationDTO.StartDate, examinationDTO.EndDate);
+            int id = 1;
+
+            foreach (Appointment app in appointments)
+                foreach (Room room in examinationDTO.Rooms)
+                {
+                    Examination examination = new Examination(id, app, ExaminationType.General, ExaminationStatus.Created,
+                                                              examinationDTO.Patient, examinationDTO.Doctor, room);
+
+                    if (examination.IsAvailable()) 
+                        examinations.Add(examination);
+                }
+            
+
+            return examinations;
+        }
+
+        private ICollection<Appointment> GenerateAppointments(DateTime startDate, DateTime endDate)
+        {
+            ICollection<Appointment> appointments = new List<Appointment>();
+            Appointment startAppointment = new Appointment(startDate);
+            Appointment endAppointment = new Appointment(endDate);
+
+            for (Appointment app = startAppointment; app.CompareTo(endAppointment) < 0; app = app.NextAppointment())
+                appointments.Add(app);
+
+            return appointments;
         }
     }
 }

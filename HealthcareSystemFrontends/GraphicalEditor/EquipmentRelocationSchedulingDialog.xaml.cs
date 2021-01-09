6ad21f4ac6ab0d35c708fccc72645796e646253e
VasilijeBursac <vasilijebursac@gmail.com>
2021-01-09 21:40:40 +0100

    gui[g4.4]: Entry data for equipment relocation

--- HealthcareSystemFrontends/GraphicalEditor/EquipmentRelocationSchedulingDialog.xaml.cs
+++ HealthcareSystemFrontends/GraphicalEditor/EquipmentRelocationSchedulingDialog.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using GraphicalEditor.Service;
 using GraphicalEditor.DTO;
 
 namespace GraphicalEditor
@@ -20,32 +21,83 @@ namespace GraphicalEditor
     /// </summary>
     public partial class EquipmentRelocationSchedulingDialog : Window
     {
-        public EquipmentWithRoomDTO EquipmentForRelocationWithRoom { get; set; }
-
+        public int EquipmentId { get; set; }
+        public string EquipmentName { get; set; }
+        public int EquipmentQuantityInStartingRoom { get; set; }
+        public int StartingRoomNumber { get; set; }
 
         public EquipmentRelocationSchedulingDialog(EquipmentWithRoomDTO equipmentWithRoomDTO)
         {
             InitializeComponent();
 
-            EquipmentForRelocationWithRoom = equipmentWithRoomDTO;
+            EquipmentId = equipmentWithRoomDTO.IdEquipment;
+            EquipmentName = equipmentWithRoomDTO.EquipmentName;
+            EquipmentQuantityInStartingRoom = equipmentWithRoomDTO.Quantity;
+            StartingRoomNumber = equipmentWithRoomDTO.RoomNumber;
 
             SetDataToUIControls();
+
+            AlternativeRelocationAppointmentsTextBlock.Visibility = Visibility.Collapsed;
+            AlternativeRelocationAppointmentsDataGrid.Visibility = Visibility.Collapsed;
+            EquipmentRelocationBackToTopButton.Visibility = Visibility.Collapsed;
+
+           
         }
 
         private void SetDataToUIControls()
         {
-            EquipmentForRelocationComboBox.Items.Add(EquipmentForRelocationWithRoom.EquipmentName);
-            RelocationStartingPointRoomComboBox.Items.Add(EquipmentForRelocationWithRoom.RoomNumber);
+            EquipmentForRelocationComboBox.Items.Add(EquipmentName);
+            RelocationStartingPointRoomComboBox.Items.Add(StartingRoomNumber);
 
-            for (int i = 1; i <= EquipmentForRelocationWithRoom.Quantity; i++)
+            SetDataToEquipmentQuantityComboBox();
+            SetDataToDestinationRoomComboBox();
+        }
+
+        private void SetDataToEquipmentQuantityComboBox()
+        {
+            for (int i = 1; i <= EquipmentQuantityInStartingRoom; i++)
             {
                 EquipmentQuantityForRelocationComboBox.Items.Add(i);
             }
         }
 
+        private void SetDataToDestinationRoomComboBox()
+        {
+            RoomService roomService = new RoomService();
+            List<RoomDTO> possibleDestinationRooms = new List<RoomDTO>();
+            foreach (RoomDTO roomDTO in roomService.GetAllRooms())
+            {
+                if (roomDTO.Id != StartingRoomNumber)
+                {
+                    possibleDestinationRooms.Add(roomDTO);
+                }
+            }
+
+            RelocationDestinationRoomComboBox.ItemsSource = possibleDestinationRooms;
+        }
+
+        private void AlternativeRelocationAppointmentsDataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void EquipmentRelocationBackToTopButton_Click(object sender, RoutedEventArgs e)
+        {
+            EquipmentRelocationScrollViewer.ScrollToTop();
+        }
+
         private void CloseButton_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+
+        private void ScheduleEquipmentRelocationButton_Click(object sender, RoutedEventArgs e)
+        {
+            AlternativeRelocationAppointmentsTextBlock.Visibility = Visibility.Visible;
+            AlternativeRelocationAppointmentsDataGrid.Visibility = Visibility.Visible;
+            EquipmentRelocationBackToTopButton.Visibility = Visibility.Visible;
+
+            EquipmentRelocationScrollViewer.ScrollToBottom();
         }
     }
 }

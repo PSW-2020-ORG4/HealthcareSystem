8cba7a3cbb3609ba855617e2a087abf1c535f4da
Ana Perisic <anaperisictb@yahoo.com>
2021-01-09 13:50:25 +0100

    feat[d64]: added service layer

---
+++ HealthcareSystem/EventSourcingService/Service/EventStoreService.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using EventSourcingService.Model;
+using EventSourcingService.Repository;
+
+namespace EventSourcingService.Service
+{
+    public class EventStoreService : IEventStoreService
+    {
+        private readonly IEventStoreRepository _eventStoreRepository;
+
+        public EventStoreService(IEventStoreRepository eventStoreRepository)
+        {
+            _eventStoreRepository = eventStoreRepository;
+        }
+
+        public IEnumerable<CustomEvent> GetAllEvents()
+        {
+            return _eventStoreRepository.GetAllEvents();
+        }
+
+        public void Add(CustomEvent customEvent)
+        {
+            _eventStoreRepository.Add(customEvent);
+        }
+    }
+}
---
+++ HealthcareSystem/EventSourcingService/Service/IEventStoreService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using EventSourcingService.Model;
+
+namespace EventSourcingService.Service
+{
+    public interface IEventStoreService
+    {
+        public IEnumerable<CustomEvent> GetAllEvents();
+
+        public void Add(CustomEvent customEvent);
+    }
+}

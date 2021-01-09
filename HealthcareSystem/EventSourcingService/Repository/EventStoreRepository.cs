60c759adc8fbe7a7cf4d92e2e2159f6bac9ddd7f
Ana Perisic <anaperisictb@yahoo.com>
2021-01-09 13:45:05 +0100

    feat[d64]: added repository layer

---
+++ HealthcareSystem/EventSourcingService/Repository/EventStoreRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using EventSourcingService.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace EventSourcingService.Repository
+{
+    public class EventStoreRepository : IEventStoreRepository
+    {
+        private readonly CustomDbContext _context;
+        public EventStoreRepository(CustomDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<CustomEvent> GetAllEvents()
+        {
+           return _context.CustomEvents.ToList();
+        }
+
+        public void Add(CustomEvent customEvent)
+        {
+            _context.CustomEvents.Add(customEvent);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ HealthcareSystem/EventSourcingService/Repository/IEventStoreRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EventSourcingService.Model;
+
+namespace EventSourcingService.Repository
+{
+    public interface IEventStoreRepository
+    {
+        IEnumerable<CustomEvent> GetAllEvents();
+
+        void Add(CustomEvent customEvent);
+    }
+}

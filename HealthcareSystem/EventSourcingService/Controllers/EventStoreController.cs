553ba2ee6cb4f4522f16a123956579c87953d85e
Ana Perisic <anaperisictb@yahoo.com>
2021-01-09 14:00:31 +0100

    feat[d64]: added controller layer

---
+++ HealthcareSystem/EventSourcingService/Controllers/EventStoreController.cs
@@ -0,0 +1,33 @@
+﻿using EventSourcingService.Model;
+using EventSourcingService.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EventSourcingService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventStoreController : ControllerBase
+    {
+        private readonly IEventStoreService _eventStoreService;
+
+        public EventStoreController(IEventStoreService eventStoreService)
+        {
+            _eventStoreService = eventStoreService;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var eventStores = _eventStoreService.GetAllEvents();
+            return Ok(eventStores);
+        }
+
+        [HttpPost]
+        public ActionResult Add(CustomEvent customEvent)
+        {
+            _eventStoreService.Add(customEvent);
+            return NoContent();
+        }
+
+    }
+}

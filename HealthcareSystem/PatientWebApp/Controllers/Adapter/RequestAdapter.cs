db64541edef366903fb6fae8713f3a99c6398bf1
Sladjana Savkovic <sladjana.savkovic97@uns.ac.rs>
2021-01-10 15:45:21 +0100

    refactor[]:rest request adapter added

---
+++ HealthcareSystem/PatientWebApp/Controllers/Adapter/RequestAdapter.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RestSharp;
+
+namespace PatientWebApp.Controllers.Adapter
+{
+    public class RequestAdapter
+    {
+        public static ContentResult SendGetRequest(string baseUrl, string resource)
+        {
+            var client = new RestClient(baseUrl);
+            var request = new RestRequest(resource);
+            var response = client.Execute(request);
+
+            var contentResult = new ContentResult();
+            contentResult.Content = response.Content;
+            contentResult.ContentType = "application/json";
+            contentResult.StatusCode = (int)response.StatusCode;
+
+            return contentResult;
+        }
+    }
+}

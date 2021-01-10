9869dca2ec7846ad5d47ccead72d29269cc3a1ad
milana-todorovic <milanatodorovic@uns.ac.rs>
2021-01-10 21:21:17 +0100

    fix: build issue fixed

--- HealthcareSystem/GraphicalEditorServer/DTO/EmergencyExaminationDTO.cs
+++ HealthcareSystem/GraphicalEditorServer/DTO/EmergencyExaminationDTO.cs
@@ -1,9 +1,5 @@
-﻿using GraphicalEditorServer.Mappers;
-using Model.PerformingExamination;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Backend.Model.PerformingExamination;
+using GraphicalEditorServer.Mappers;
 
 namespace GraphicalEditorServer.DTO
 {

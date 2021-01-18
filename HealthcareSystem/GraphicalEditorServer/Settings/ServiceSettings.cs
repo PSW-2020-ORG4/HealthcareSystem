1d9fc5e4ff1e38299ac38f306ca3ae3ef29909d7
VasilijeBursac <vasilijebursac@gmail.com>
2021-01-18 14:38:56 +0100

    feat[g5.ddd]: Event Sourcing on Frontend; Connection with Backend

--- HealthcareSystem/GraphicalEditorServer/Settings/ServiceSettings.cs
+++ HealthcareSystem/GraphicalEditorServer/Settings/ServiceSettings.cs
@@ -8,5 +8,7 @@ namespace GraphicalEditorServer.Settings
     public class ServiceSettings
     {
         public string PatientServiceUrl { get; set; }
+
+        public string EventSourcingServiceUrl { get; set; }
     }
 }
--- HealthcareSystemFrontends/GraphicalEditor/MainWindow.xaml.cs
+++ HealthcareSystemFrontends/GraphicalEditor/MainWindow.xaml.cs
@@ -31,7 +31,7 @@ namespace GraphicalEditor
         public static List<MapObject> _allMapObjects;
         public static List<MapObjectType> _allMapObjectTypes;
         private string _currentUserRole;
-
+        public static string _currentUsername = "";
 
         private IRepository _fileRepository;
 
@@ -190,12 +190,13 @@ namespace GraphicalEditor
         }
         
 
-        public MainWindow(string currentUserRole)
+        public MainWindow(string currentUserRole, string currentUsername)
         {
             InitializeComponent();
             this.DataContext = this;
             AllMapObjectTypes = MapObjectType.AllMapObjectTypesAvailableForSearch;
             _currentUserRole = currentUserRole;
+            _currentUsername = currentUsername;
             _canvas = this.Canvas;
             _fileRepository = new FileRepository("test.json");
             _mapObjectController = new MapObjectController(new MapObjectServices(_fileRepository));
@@ -212,6 +213,8 @@ namespace GraphicalEditor
             RestrictUsersAccessBasedOnRole();
 
             SetDataToUIControls();
+
+            Console.WriteLine("Korisnicko ime " + _currentUsername);
         }
 
         public void SetDataToUIControls()
@@ -439,6 +442,7 @@ namespace GraphicalEditor
                 ObjectEquipmentDataGrid.ItemsSource = SelectedMapObject.GetEquipmentInObject();
                 ObjectMedicineDataGrid.ItemsSource = SelectedMapObject.GetMedicineInObject();
 
+                AddEventsForEventSourcing(selectedMapObjectForDisplay);
             }
             else
             {
@@ -446,6 +450,19 @@ namespace GraphicalEditor
                 DisplayMapObject = null;
             }
         }
+
+        private void AddEventsForEventSourcing(MapObject selectedMapObject)
+        {
+            if (selectedMapObject.CheckIfDBAddableRoom())
+            {
+                ((Room)selectedMapObject.MapObjectEntity).AddRoomSelectionEvent();
+            }
+            else if (selectedMapObject.MapObjectEntity.MapObjectType.TypeOfMapObject == TypeOfMapObject.BUILDING)
+            {
+                ((Building)selectedMapObject.MapObjectEntity).AddBuildingSelectionEvent();
+            }
+        }
+
 
         private void ApplyHoverEffectToObject(MapObject hoverMapObject)
         {

5668f4083925575b8268fa5148d22d4e1b50d3a6
Sladjana Savkovic <sladjana.savkovic97@uns.ac.rs>
2021-01-19 20:20:25 +0100

    refactor[w5.4]:class packaging

---
+++ HealthcareSystem/EventSourcingService/Service/PatientSchedulingEventService/PatientSchedulingStatisticalService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EventSourcingService.Service
+{
+    public class PatientSchedulingStatisticalService : IPatientSchedulingStatisticalService
+    {
+    }
+}

c0adc6468da866093b9680a4f31b51e0fe6b109e
borislav-puzic <borislavpuzic@gmail.com>
2021-01-25 13:16:00 +0100

    refactor[ia]: tests refactord because of microservices

--- HealthcareSystem/IntegrationAdaptersTests/IntegrationTests/ActionBenefitServiceTest.cs
+++ HealthcareSystem/IntegrationAdaptersTests/IntegrationTests/ActionBenefitServiceTest.cs
@@ -1,28 +1,27 @@
-﻿// using Backend.Model;
-// using Backend.Repository;
-// using Backend.Service;
-// using System;
-// using Xunit;
+﻿using Backend.Model;
+using Backend.Model.Exceptions;
+using IntegrationAdaptersActionBenefitService.Repository;
+using IntegrationAdaptersActionBenefitService.Service;
+using Xunit;
 
-// namespace IntegrationAdaptersTests.IntegrationTests
-// {
-//     public class ActionBenefitServiceTest
-//     {
-//         [Fact]
-//         public void CreateActionBenefit_Valid_Invalid_Test()
-//         {
-//             DbContextInMemory testData = new DbContextInMemory();
-//             MyDbContext context = testData._context;
-//             var pharmacyRepo = new MySqlPharmacyRepo(context);
-//             var actionBenefitRepo = new MySqlActionBenefitRepository(context);
-//             var actionBenefitService = new ActionBenefitService(actionBenefitRepo, pharmacyRepo);
-//             var message = new ActionBenefitMessage { Subject = "akcija1", Message = "blablabla" };
-            
-//             actionBenefitService.CreateActionBenefit("exchange", message);
-//             Assert.Contains(context.ActionsBenefits, ab => ab.Message == message.Message && ab.Subject == message.Subject);
+namespace IntegrationAdaptersTests.IntegrationTests
+{
+    public class ActionBenefitServiceTest
+    {
+        [Fact]
+        public void CreateActionBenefit_Valid_Invalid_Test()
+        {
+            DbContextInMemory testData = new DbContextInMemory();
+            MyDbContext context = testData._context;
+            var pharmacyRepo = new MySqlPharmacyRepo(context);
+            var actionBenefitRepo = new MySqlActionBenefitRepository(context);
+            var actionBenefitService = new ActionBenefitService(actionBenefitRepo, pharmacyRepo);
+            var message = new ActionBenefitMessage("akcija1", "blablabla");
 
-//             message = new ActionBenefitMessage { Subject = "", Message = "" };
-//             Assert.Throws<ArgumentException>(() => actionBenefitService.CreateActionBenefit("exchange", message));
-//         }
-//     }
-// }
+            actionBenefitService.CreateActionBenefit("exchange", message);
+            Assert.Contains(context.ActionsBenefits, ab => ab.Message.Message == message.Message && ab.Message.Subject == message.Subject);
+
+            Assert.Throws<ValidationException>(() => new ActionBenefitMessage("", ""));
+        }
+    }
+}
--- HealthcareSystem/IntegrationAdaptersTests/UnitTests/EditPharmacyTests.cs
+++ HealthcareSystem/IntegrationAdaptersTests/UnitTests/EditPharmacyTests.cs
@@ -1,43 +1,44 @@
-﻿// using Backend.Repository;
-// using Backend.Service;
-// using Backend.Service.Pharmacies;
-// using IntegrationAdapters.Controllers;
-// using IntegrationAdaptersTests.DataFactory;
-// using Microsoft.AspNetCore.Mvc;
-// using Moq;
-// using Xunit;
+﻿using Backend.Model.Pharmacies;
+using IntegrationAdapters.Controllers;
+using IntegrationAdapters.MicroserviceComunicator;
+using IntegrationAdaptersTests.DataFactory;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System.Threading.Tasks;
+using Xunit;
 
-// namespace IntegrationAdaptersTests.UnitTests
-// {
-//     public class EditPharmacyTests
-//     {
-//         [Fact]
-//         public void Finds_pharmacy_to_edit()
-//         {
-//             var controller = GetPharmacyController();
-//             var result = controller.Edit(1);
+namespace IntegrationAdaptersTests.UnitTests
+{
+    public class EditPharmacyTests
+    {
+        [Fact]
+        public async void Finds_pharmacy_to_edit()
+        {
+            var controller = GetPharmacyController();
+            var result = await controller.Edit(1);
 
-//             Assert.IsType<ViewResult>(result);
-//         }
+            Assert.IsType<ViewResult>(result);
+        }
 
-//         [Fact]
-//         public void Does_not_find_pharmacy_to_edit()
-//         {
-//             var controller = GetPharmacyController();
-//             var result = controller.Edit(2);
+        [Fact]
+        public async void Does_not_find_pharmacy_to_edit()
+        {
+            var controller = GetPharmacyController();
+            var result = await controller.Edit(2);
 
-//             Assert.IsType<NotFoundObjectResult>(result);
-//         }
+            Assert.IsType<NotFoundObjectResult>(result);
+        }
 
-//         private PharmacyController GetPharmacyController()
-//         {
-//             var stubRepository = new Mock<IPharmacyRepo>();
-//             var mockRabbit = new Mock<IRabbitMqActionBenefitService>();
-//             var pharmacy = CreatePharmacy.CreateValidTestObject();
-//             pharmacy.Id = 1;
+        private PharmacyController GetPharmacyController()
+        {
+            var pharmacy = CreatePharmacy.CreateValidTestObject();
+            pharmacy.Id = 1;
+            PharmacySystem pharmacyNull = null;
+            var pharmacyServiceMock = new Mock<IPharmacySystemService>();
+            pharmacyServiceMock.Setup(p => p.Get(It.Is<int>(i => i == 1))).Returns(Task.Run(() => pharmacy));
+            pharmacyServiceMock.Setup(p => p.Get(It.Is<int>(i => i != 1))).Returns(Task.Run(() => pharmacyNull));
 
-//             stubRepository.Setup(m => m.GetPharmacyById(pharmacy.Id)).Returns(pharmacy);
-//             return new PharmacyController(new PharmacyService(stubRepository.Object), mockRabbit.Object);
-//         }
-//     }
-// }
+            return new PharmacyController(pharmacyServiceMock.Object);
+        }
+    }
+}
